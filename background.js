6ce33486d2880bfec92607090d85e9638cdc3355
Adam Bernstein <abernstein@pih.org>
2017-01-24 15:34:52 -0500

    Working with good error checks. Need to update to get image source instead of linkUrl

--- background.js
+++ background.js
@@ -1,15 +1,27 @@
+//Create the menu item, but only on instagram.com
 chrome.contextMenus.create({
-  id: "clickme",
-  title: "Click me!",
+  id: "imget",
+  title: "IMGet!",
   contexts: ["all"], 
-  documentUrlPatterns: ["http://example.org/*"]
+  documentUrlPatterns: ["*://*.instagram.com/*"]
 });
 
-
+//Add listener for menu item click. 'info' about the click target included 
 browser.contextMenus.onClicked.addListener((info, tab) => {
+	//if the click target has no srcUrl, give an alert
+	if (info.linkUrl == "") {
+		console.log(`Error: linkUrl is blank`);	
+		
+		var sendAlert = 'alert("No image selected")';
+		
+		//using the new js "Promise"
+		var executing = browser.tabs.executeScript({
+			code: sendAlert
+		});
+		executing.then(onExecuted, onError);
+	}
 	
-	if (info.linkUrl == "") console.log(`Error: linkUrl is blank`);	
-	
+	//else create a new tab with the location of the srcUrl
 	else {
 		var creating = browser.tabs.create({
 			url:info.linkUrl
@@ -24,4 +36,8 @@ function onCreated(tab) {
 
 function onError(error) {
   console.log(`Error: ${error}`);
+}
+
+function onExecuted(result){
+	console.log(`Alert sent to current tab ${result}`);
 }
